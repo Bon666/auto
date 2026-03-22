76f7434c3a6e702f673b782b15d41b26fc52a872
Bon666 <bondezhang66@gmail.com>
2026-03-21 18:30:18 -0700

    Add files via upload

---
+++ userid.js
@@ -0,0 +1,5 @@
+const userId = //yourid save with int, DO NOT save with string.;
+
+// Get userid: 
+// Enter window.App.context.dispatcher.stores.ApplicationStore.userData.id in your browser console
+// and copy the value of id
