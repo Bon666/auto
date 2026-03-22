8ca1f2dfa922bd3b5e588cc64486f605e5710812
Bon666 <bondezhang66@gmail.com>
2026-03-21 18:30:43 -0700

    Update userid.js

--- userid.js
+++ userid.js
@@ -1,5 +1,1 @@
-const userId = //yourid save with int, DO NOT save with string.;
-
-// Get userid: 
-// Enter window.App.context.dispatcher.stores.ApplicationStore.userData.id in your browser console
-// and copy the value of id
+207743887
